bed728d4cf339c182b5fe95d6bfbdeb64de17e52
Daniele <daniele.stirp@gmail.com>
2024-12-16 11:34:57 +0100

    Print hidden files using print_top_level function

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from src.pyls import print_top_level
 if __name__ == '__main__':
     with open("./data/sample.json") as f:
         data = json.load(f)
-    print_top_level(data)
+    print_top_level(data, True)
 
 
 
--- src/pyls.py
+++ src/pyls.py
@@ -1,8 +1,11 @@
 
-def print_top_level(json_file):
+def print_top_level(json_file, print_hidden):
     result = ""
     if "contents" in json_file:
         for item in json_file["contents"]:
-            if item["name"][0] != ".":
+            if not print_hidden:
+                if item["name"][0] != ".":
+                    result = result + " " + item["name"]
+            else:
                 result = result + " " + item["name"]
     print(result)
