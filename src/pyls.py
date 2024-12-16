f8ed2ea98aefebecebe4e9d343e6a91d45743822
Daniele <daniele.stirp@gmail.com>
2024-12-16 16:18:19 +0100

    Print vertically function: store items in list of dict in such a way to include files with same timestamp + Move __main__ in pyls module as per specs + Add .gitignore file

--- src/pyls.py
+++ src/pyls.py
@@ -1,6 +1,8 @@
-import datetime, collections
+import sys, datetime
+import json
+from operator import itemgetter
 
-def print_top_level(json_file, print_hidden):
+def print_top_level(json_file, print_hidden=False):
     result = ""
     if "contents" in json_file:
         for item in json_file["contents"]:
@@ -12,7 +14,7 @@ def print_top_level(json_file, print_hidden):
     print(result)
 
 def print_top_level_vertically_with_info(json_file, print_hidden=False, reverse=False, time_sorted=False):
-    my_dict = dict()
+    my_list = list()
     if "contents" in json_file:
         if reverse:
             my_json_list = reversed(json_file["contents"])
@@ -28,14 +30,41 @@ def print_top_level_vertically_with_info(json_file, print_hidden=False, reverse=
             if not print_hidden:
                 if item["name"][0] != ".":
                     current_str = item["permissions"] + " " + str(item["size"]) + " " + my_date + " " +item["name"]
-                    my_dict[item["time_modified"]] = current_str
+                    my_dict = {
+                        "timestamp": item["time_modified"],
+                        "info": current_str
+                    }
+                    my_list.append(my_dict)
             else:
                 current_str = item["permissions"] + " " + str(item["size"]) + " " + my_date + " " +item["name"]
-                my_dict[item["time_modified"]] = current_str
+                my_dict = {
+                    "timestamp": item["time_modified"],
+                    "info": current_str
+                }
+                my_list.append(my_dict)
     if time_sorted:
         if reverse:
-            my_dict = collections.OrderedDict(sorted(my_dict.items(), reverse=reverse))
+            my_list = sorted(my_list, key=itemgetter("timestamp"), reverse=True)
         else:
-            my_dict = collections.OrderedDict(sorted(my_dict.items()))
-    for index, item in my_dict.items():
-        print(item)
+            my_list = sorted(my_list, key=itemgetter("timestamp"))
+    for item in my_list:
+        print(item["info"])
+
+if __name__ == '__main__':
+    print("Start")
+    args = sys.argv
+    with open("../data/sample.json") as f:
+        data = json.load(f)
+    print(args)
+    if len(args) == 1:
+        print_top_level(data)
+    elif len(args) == 2 and '-A' in args:
+        print_top_level(data, print_hidden=True)
+    elif len(args) == 2 and '-l' in args:
+        print_top_level_vertically_with_info(data)
+    elif len(args) == 3 and '-l' in args and '-r' in args:
+        print_top_level_vertically_with_info(data, reverse=True)
+    elif len(args) == 3 and '-l' in args and '-t' in args:
+        print_top_level_vertically_with_info(data, time_sorted=True)
+    elif len(args) == 4 and '-l' in args and '-r' in args and '-t' in args:
+        print_top_level_vertically_with_info(data, reverse=True, time_sorted=True)
