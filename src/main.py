1b55f36180687fd58191f3c67c6e521f1954d2cf
Daniele <daniele.stirp@gmail.com>
2024-12-16 12:02:46 +0100

    Function print top level vertically with additional info (together with hidden file print feature)

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 import json
-from src.pyls import print_top_level
+from src.pyls import print_top_level, print_top_level_vertically_with_info
 
 if __name__ == '__main__':
     with open("./data/sample.json") as f:
         data = json.load(f)
     print_top_level(data, True)
+    print()
+    print_top_level_vertically_with_info(data, True)
 
 
 
--- src/pyls.py
+++ src/pyls.py
@@ -1,3 +1,4 @@
+import datetime
 
 def print_top_level(json_file, print_hidden):
     result = ""
@@ -9,3 +10,18 @@ def print_top_level(json_file, print_hidden):
             else:
                 result = result + " " + item["name"]
     print(result)
+
+def print_top_level_vertically_with_info(json_file, print_hidden):
+    if "contents" in json_file:
+        for item in json_file["contents"]:
+            date = datetime.datetime.fromtimestamp(item["time_modified"])
+            month = date.strftime('%b')
+            day = date.day
+            hour = date.hour
+            minute = date.minute
+            my_date = str(month) + " " + str(day) + " " + str(hour) + ":" + str(minute)
+            if not print_hidden:
+                if item["name"][0] != ".":
+                    print(item["permissions"] + " " + str(item["size"]) + " " + my_date + " " +item["name"])
+            else:
+                print(item["permissions"] + " " + str(item["size"]) + " " + my_date + " " + item["name"])
