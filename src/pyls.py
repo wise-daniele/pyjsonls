7084e8abda707d0d353cdb7cf54e6c51737c308a
Daniele <daniele.stirp@gmail.com>
2024-12-17 10:39:09 +0100

    Apply filter for both horizontal and vertical print + rename some variables

--- src/pyls.py
+++ src/pyls.py
@@ -4,32 +4,36 @@ import argparse
 from operator import itemgetter
 
 
-def print_top_level(json_file, all=False):
+def print_top_level(data, print_all=False, filter=None):
     result = ""
-    if "contents" in json_file:
-        for item in json_file["contents"]:
-            if not all:
+    if "contents" in data:
+        for item in data["contents"]:
+            if apply_filter(item, filter):
+                continue
+            if not print_all:
                 if item["name"][0] != ".":
                     result = result + " " + item["name"]
             else:
                 result = result + " " + item["name"]
     print(result)
 
-def print_top_level_vertically_with_info(json_file, all=False, reverse=False, time_sorted=False, filter=None):
+def print_top_level_vertically_with_info(data, print_all=False, reverse=False, time_sorted=False, my_filter=None):
     my_list = list()
-    if "contents" in json_file:
+    if "contents" in data:
         if reverse:
-            my_json_list = reversed(json_file["contents"])
+            my_json_list = reversed(data["contents"])
         else:
-            my_json_list = json_file["contents"]
+            my_json_list = data["contents"]
         for item in my_json_list:
+            if apply_filter(item, my_filter):
+                continue
             date = datetime.datetime.fromtimestamp(item["time_modified"])
             month = date.strftime('%b')
             day = date.day
             hour = date.hour
             minute = date.minute
             my_date = str(month) + " " + str(day) + " " + str(hour) + ":" + str(minute)
-            if not all:
+            if not print_all:
                 if item["name"][0] != ".":
                     current_str = item["permissions"] + " " + str(item["size"]) + " " + my_date + " " +item["name"]
                     my_dict = {
@@ -52,10 +56,16 @@ def print_top_level_vertically_with_info(json_file, all=False, reverse=False, ti
     for item in my_list:
         print(item["info"])
 
+def apply_filter(item, my_filter):
+    if my_filter:
+        if (my_filter == "dir" and "contents" not in item) or (my_filter == "file" and "contents" in item):
+            return True
+    return False
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-A', '--all', action=argparse.BooleanOptionalAction,
-                        help="Prints all top level files and directories", default=False)
+    parser.add_argument('-A', '--print_all', action=argparse.BooleanOptionalAction,
+                        help="Prints all files and directories (only top level)", default=False)
     parser.add_argument('-l', '--vert', action=argparse.BooleanOptionalAction,
                         help="Prints the results vertically with additional information",
                         default=False)
@@ -78,6 +88,6 @@ if __name__ == '__main__':
         data = json.load(f)
 
     if not args.vert:
-        print_top_level(data, args.all)
+        print_top_level(data, args.print_all, args.filter)
     else:
-        print_top_level_vertically_with_info(data, args.all, args.reverse, args.time, args.filter)
+        print_top_level_vertically_with_info(data, args.print_all, args.reverse, args.time, args.filter)
