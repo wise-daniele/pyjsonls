64f0c9619e19d0c805de6b4953f20100374ba6cd
Daniele <daniele.stirp@gmail.com>
2024-12-17 20:55:51 +0100

    Add project script entry in .toml + create a main function to test from command line after setup

--- src/pyls/pyls.py
+++ src/pyls/pyls.py
@@ -146,7 +146,7 @@ def apply_filter(item, my_filter):
             return True
     return False
 
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser("This program takes a json file containing the representation of a directory"
                                      " in nested structure, and prints out its content in the console in the style"
                                      " of ls (linux utility).")
@@ -175,6 +175,45 @@ if __name__ == '__main__':
 
     print(args)
     with open("../../data/sample.json") as f:
+        json_data = json.load(f)
+
+    if not args.vert:
+        print_top_level(json_data, args.print_all, args.filter)
+    else:
+        if args.path:
+            print_path_info(json_data, args.print_all, args.reverse, args.time, args.filter, args.path)
+        else:
+            print_top_level_vertically_with_info(json_data, args.print_all, args.reverse, args.time, args.filter)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("This program takes a json file containing the representation of a directory"
+                                     " in nested structure, and prints out its content in the console in the style"
+                                     " of ls (linux utility).")
+    parser.add_argument('-A', '--print_all', action='store_true',
+                        help="Prints all files and directories at the top level. If path argument is specified"
+                             "prints all files and directories at the path sub level")
+    parser.add_argument('-l', '--vert', action='store_true',
+                        help="Prints the results vertically with additional information")
+    parser.add_argument('-r', '--reverse', action='store_true',
+                        help="Prints the results vertically with additional information in reverse"
+                             " order with respect to the json list. If used with the '-t' argument,"
+                             " prints the result in reverse order with respect to the modified time",
+                        default=False)
+    parser.add_argument('-t', '--time', action='store_true',
+                        help="Prints the results sorted by the modified time. If used with '-r' "
+                             "argument, prints the result in reverse order with respect to the "
+                             "modified time")
+    parser.add_argument('--filter', type=str, choices=['dir', 'file'],
+                        help="Filters the output according to a given option. The available options"
+                             " are 'dir' and 'file'")
+    parser.add_argument('path', nargs='?',
+                        help="Navigate the structure within the json and prints the file information"
+                             " if the path represents a file, and the list of contents with their"
+                             " relative information if the path is a directory", default=None)
+    args = parser.parse_args()
+
+    print(args)
+    with open("../data/sample.json") as f:
         json_data = json.load(f)
 
     if not args.vert:
